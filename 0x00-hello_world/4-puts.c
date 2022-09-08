a9726ca14e8f08e6841b31a1a92d299e64801be3
unyime <udeng45@gmail.com>
2022-09-08 16:03:09 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - Entry point
  * 
- * Return: 0 After printing the statement
+ * Return: 0 (SUCCESS)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+  puts("\"Programming is like building a multilingual puzzle");
+   return (0);
 }
