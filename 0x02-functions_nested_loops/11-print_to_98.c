6cbc8b4f8ddbeaa912406ce66feacedfb6a5f939
unyime <udeng45@gmail.com>
2022-09-15 16:18:02 -0700

    98 degrees

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,11 +2,11 @@ i#include <stdio.h>
 #include "main.h"
 
 /**
- *  * print_to_98 - Check Holberton
- *   * @n: A input integer
- *    * Description: function that prints all natural numbers
- *     * from n to 98, followed by a new line.
- *      * Return: Nothing
+ *   print_to_98 - Check Holberton
+ *    @n: A input integer
+ *     Description: function that prints all natural numbers
+ *      from n to 98, followed by a new line.
+ *       Return: Nothing
  */
 void print_to_98(int n)
 {
