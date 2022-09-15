2d82832a690b3bfe23692f2c00a13ec8449a3c59
unyime <udeng45@gmail.com>
2022-09-15 14:06:24 -0700

    program that prints _putchar, followed by a new line.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,11 +7,12 @@
  * Description: prints _putchar
  *
  * Return: Always 0 (Success)
- * Return: 0 (Success)
+ *
  */
 int main(void)
 {
 	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+
 	int count sz;
 
 	sz = sizeof(str) / sizeof(int);
