7ce434f946d25ec188037fc9022cfc4b03280932
unyime <udeng45@gmail.com>
2022-09-15 16:53:14 -0700

    supper

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,14 +8,14 @@
  * Return: Always 0 (Success)
  */
 
-int main (void)
+int main(void)
 {
-	int d; p; q;
+int d; p; q;
 
 	for (d = '0'; d < '9'; d++)
 	{
 	         for (p = d + 1; p <= '9'; p++)
-		 {
+	 {
 			 for (q = p + 1; q <= '9'; q++)
 			 {
 				 if ((p != d) != q)
