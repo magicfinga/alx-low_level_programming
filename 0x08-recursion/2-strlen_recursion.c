ed818ff9c1ff9272fa0fa84f819c746b56b6f06d
unyime udeng <udeng45@gmail.com>
2022-09-28 07:59:47 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,7 +4,7 @@
  *  * _strlen_recursion - returns the length of a string.
  *   * @s: string
  *    * Return: the length of a string.
- *     */
+ */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
